eb5429bd71ce09d70d8725972347dbce06383e74
PMH <phumon91@gmail.com>
2017-01-24 23:20:06 +0800

    Updated

--- quickstart.py
+++ quickstart.py
@@ -2,6 +2,7 @@ import sys
 from wit import Wit
 
 if len(sys.argv) != 2:
+    print('usage: python ' + sys.argv[0] + ' <wit-token>')
     exit(1)
 access_token = sys.argv[1]
 
@@ -22,25 +23,23 @@ def send(request, response):
 def get_forecast(request):
     context = request['context']
     entities = request['entities']
-    print(context)
-    print(entities)
 
     loc = first_entity_value(entities, 'location')
     if loc:
-        context['forecast'] = 'cloudy'
+        context['forecast'] = 'sunny'
+        if context.get('missingLocation') is not None:
+            del context['missingLocation']
     else:
-        context['forecast'] = 'rainy'
         context['missingLocation'] = True
- 
+        if context.get('forecast') is not None:
+            del context['forecast']
+
     return context
 
 actions = {
     'send': send,
-    'getForecast': get_forecast
+    'getForecast': get_forecast,
 }
 
 client = Wit(access_token=access_token, actions=actions)
-session_id = 'my-user-session-42'
-context0 = {}
-context1 = client.run_actions(session_id, 'What is the forecast in Yangon?', context0)
-print('The session state is now: ' + str(context1))
+client.interactive()
